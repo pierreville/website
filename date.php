d81f9de2d99f90cb83f4bdd5eb67730b7ba93a25
Pierre Bastien <pierreville@gmail.com>
2019-07-03 16:57:24 -0400

    New version of site

--- date.php
+++ date.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 
 <html>
-<head></head>
+<head>
+	<title>Date</title>
+</head>
 <body class="page_bg">
 Hello, today is <?php echo date('l, F jS, Y'); ?>.
 </body>
-</html>
 </html>
